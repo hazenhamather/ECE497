4a4202d0a1d13166c58b8221e49f8d60ff978e37
root <root@beaglebone.localdomain>
2016-09-16 05:37:59 +0000

    Submitting tmp101 file

---
+++ hw03/tmp101.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#This is the shell file for the two TMP101 temperature sensors
+
+#Setting up alerts (thermostat mode)[Polarity and thermostat high]
+i2cset -y 2 0x48 0x01 0x06
+i2cset -y 2 0x49 0x01 0x06
+
+#Setting high and low temps for both pieces
+i2cset -y 2 0x48 0x02 0x20 #Setting Low 
+i2cset -y 2 0x48 0x03 0x18 #Setting High
+
+i2cset -y 2 0x49 0x02 0x20 #Setting Low
+i2cset -y 2 0x49 0x03 0x18 #Setting High
+
+#Pointing registers back to thermostat
+i2cset -y 2 0x48 0x00
+i2cset -y 2 0x49 0x00
+
+temp1=$(i2cget -y 2 0x48)
+temp2=$(i2cget -y 2 0x49)
+
+temp1=$((temp1*9/5 + 32))
+temp2=$((temp2*9/5 + 32))
+
+echo $temp1
+echo $temp2
